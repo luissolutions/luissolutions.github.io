2d8e78987167be640a0d899c16acd537089aea02
Luis <133532790+luissolutions@users.noreply.github.com>
2023-09-19 21:48:06 -0400

    honeydo

---
+++ apps/ias/js/index.js
@@ -0,0 +1,41 @@
+const headerContainer = document.getElementById('headerContainer');
+const bodyContainer = document.getElementById('bodyContainer');
+const footerContainer = document.getElementById('footerContainer');
+
+// Function to load content into a container
+function loadContent(container, path) {
+    return fetch(path)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error fetching ${path}: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            container.innerHTML = html;
+        });
+}
+
+function handleNavClick(event) {
+    event.preventDefault();
+
+    const pageName = event.target.getAttribute('data-page');
+    loadPage(bodyContainer, `pages/${pageName}`);
+}
+
+function loadPage(container, path) {
+    loadContent(container, path)
+        .then(() => {
+            // If the header was loaded, attach event listeners to the nav links
+            if (container === headerContainer) {
+                document.querySelectorAll('[data-page]').forEach(link => {
+                    link.addEventListener('click', handleNavClick);
+                });
+            }
+        });
+}
+
+// Initial content loading
+loadPage(headerContainer, 'header.html');
+loadPage(bodyContainer, 'pages/home.html');
+loadPage(footerContainer, 'footer.html');
