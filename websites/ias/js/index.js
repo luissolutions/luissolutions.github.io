c709b07cd15ee2c12d2d08bb44ad3f31c3ea5681
Luis <133532790+luissolutions@users.noreply.github.com>
2023-09-30 16:44:48 -0400

    undo update to livenotes

---
+++ websites/ias/js/index.js
@@ -0,0 +1,23 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("data-include") || elmnt.getAttribute("id");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    elmnt.removeAttribute("data-include");
+                    elmnt.removeAttribute("id");
+                    includeHTML();
+                }
+            }      
+            xhttp.open("GET", file + '.html', true);
+            xhttp.send();
+            return;
+        }
+    }
+}
