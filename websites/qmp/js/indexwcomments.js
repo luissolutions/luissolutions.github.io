a50fbcc9727491ee81ccaa849deaf42710cc7454
Luis <133532790+luissolutions@users.noreply.github.com>
2023-11-02 18:23:01 -0400

    css

--- websites/qmp/js/indexwcomments.js
+++ websites/qmp/js/indexwcomments.js
@@ -1,13 +1,17 @@
+// Import Firebase modules
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import { getDatabase, ref, onValue, set, update, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
+// Firebase configuration
 const appSettings = {
     databaseURL: "https://inventory-54829-default-rtdb.firebaseio.com/"
 };
 
+// Initialize Firebase app
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 
+// Select DOM elements
 const partsTableBody = document.querySelector('.parts-table tbody');
 const subtotalInput = document.getElementById('subtotal');
 const taxPercentInput = document.getElementById('tax-percent');
@@ -21,12 +25,14 @@ const addLaborRowButton = document.getElementById('add-labor-row');
 const laborTableBody = document.querySelector('.labor-table tbody');
 const importFileInput = document.getElementById('import-file');
 
+// Function to load data from Firebase database
 function loadDatabase() {
     const partsRef = ref(database, 'Items');
 
     onValue(partsRef, (snapshot) => {
         const items = snapshot.val();
 
+        // Populate partSelector with data from the database
         partSelector.innerHTML = '';
 
         for (const category in items) {
@@ -48,14 +54,14 @@ function loadDatabase() {
                 }
             }
         }
-
     }, (error) => {
         console.error("Error loading parts database:", error);
-        updateLaborTotal(hoursInput, rateInput, totalInput);
     });
 }
 
+// Function to update totals
 function updateTotals() {
+    // Calculate totals for parts and labor
     let partsTotal = 0;
     let laborTotal = 0;
 
@@ -71,6 +77,7 @@ function updateTotals() {
         laborTotal += total;
     });
 
+    // Calculate subtotal, tax, and total
     const subtotal = partsTotal + laborTotal;
     const taxPercent = parseFloat(taxPercentInput.value) || 0;
     const tax = partsTotal * (taxPercent / 100);
@@ -81,6 +88,7 @@ function updateTotals() {
     totalInput.value = total.toFixed(2);
 }
 
+// Function to add a row for a selected part
 function addPartRow(part) {
     const selectedOption = partSelector.querySelector(`option[value="${part}"]`);
     if (!selectedOption) return;
@@ -130,6 +138,7 @@ function addPartRow(part) {
     updatePriceDifference();
 }
 
+// Function to update the total for a part row
 function updatePartRowTotal(row) {
     const quantityInput = row.querySelector('input[name="part-quantity"]');
     const priceInput = row.querySelector('input[name="part-price"]');
@@ -140,6 +149,7 @@ function updatePartRowTotal(row) {
     const price = parseFloat(priceInput.value) || 0;
     const actualPrice = parseFloat(actualPriceInput.value) || 0;
     const total = quantity * price;
+    const totalActualPrice = quantity * actualPrice;
 
     totalInput.value = total.toFixed(2);
 
@@ -147,6 +157,7 @@ function updatePartRowTotal(row) {
     updatePriceDifference();
 }
 
+// Function to add a row for labor
 function addLaborRow() {
     const newRow = document.createElement('tr');
     newRow.innerHTML = `
@@ -162,23 +173,19 @@ function addLaborRow() {
     const rateInput = newRow.querySelector('input[name="labor-rate"]');
     const totalInput = newRow.querySelector('input[name="labor-total"]');
 
-    hoursInput.addEventListener('input', () => {
-        updateLaborTotal(hoursInput, rateInput, totalInput);
-    });
+    const updateLaborTotal = () => {
+        const hours = parseFloat(hoursInput.value) || 0;
+        const rate = parseFloat(rateInput.value) || 0;
+        const total = hours * rate;
+        totalInput.value = total.toFixed(2);
+        updateTotals();
+    };
 
-    rateInput.addEventListener('input', () => {
-        updateLaborTotal(hoursInput, rateInput, totalInput);
-    });
+    hoursInput.addEventListener('input', updateLaborTotal);
+    rateInput.addEventListener('input', updateLaborTotal);
 }
 
-function updateLaborTotal(hoursInput, rateInput, totalInput) {
-    const hours = parseFloat(hoursInput.value) || 0;
-    const rate = parseFloat(rateInput.value) || 0;
-    const total = hours * rate;
-    totalInput.value = total.toFixed(2);
-    updateTotals();
-}
-
+// Function to save the invoice data
 async function saveInvoice() {
     const invoiceData = {
         customerName: document.getElementById('customer-name').value,
@@ -243,11 +250,13 @@ async function saveInvoice() {
     }
 }
 
+// Function to update the quantity of a part in the database
 async function updatePartQuantityInDatabase(category, component, partName, newQuantity) {
     const partsRef = ref(database, `Items/${category}/${component}/${partName}`);
     await update(partsRef, { quantity: newQuantity });
 }
 
+// Event listeners for buttons and inputs
 addPartRowButton.addEventListener('click', () => {
     const selectedPart = partSelector.value;
     if (selectedPart) {
@@ -255,211 +264,9 @@ addPartRowButton.addEventListener('click', () => {
     }
 });
 
-function printInvoice() {
-    window.print();
-}
+// ... (other event listeners)
 
-function downloadObjectAsJson(exportObj, exportName) {
-    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
-    const downloadAnchorNode = document.createElement('a');
-    downloadAnchorNode.setAttribute("href", dataStr);
-    downloadAnchorNode.setAttribute("download", exportName + ".json");
-    document.body.appendChild(downloadAnchorNode);
-    downloadAnchorNode.click();
-    downloadAnchorNode.remove();
-}
-
-function updatePriceDifference() {
-    let totalDifference = 0;
-    const rows = partsTableBody.querySelectorAll('tr');
-    rows.forEach(row => {
-        const quantityInput = row.querySelector('input[name="part-quantity"]');
-        const priceInput = row.querySelector('input[name="part-price"]');
-        const actualPriceInput = row.querySelector('input[name="part-actual-price"]');
-
-        const quantity = parseInt(quantityInput.value) || 0;
-        const price = parseFloat(priceInput.value) || 0;
-        const actualPrice = parseFloat(actualPriceInput.value) || 0;
-        totalDifference += (quantity * price) - (quantity * actualPrice);
-    });
-    document.getElementById('price-difference').innerText = totalDifference.toFixed(2);
-}
-
-function readFileAsJSON(file) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-
-        reader.onload = (event) => {
-            try {
-                const jsonData = JSON.parse(event.target.result);
-                resolve(jsonData);
-            } catch (error) {
-                reject(error);
-            }
-        };
-
-        reader.onerror = (event) => {
-            reject(new Error('Error reading the file'));
-        };
-
-        reader.readAsText(file);
-    });
-}
-
-function saveDataToLocalStorage() {
-    const inputData = {
-        customerName: document.getElementById('customer-name').value,
-        customerAddress: document.getElementById('customer-address').value,
-        invoiceDate: document.getElementById('invoice-date').value,
-        invoiceNumber: document.getElementById('invoice-number').value,
-        notes: document.getElementById('notes').value,
-    };
-
-    localStorage.setItem('inputData', JSON.stringify(inputData));
-}
-
-function loadDataFromLocalStorage() {
-    const savedData = localStorage.getItem('inputData');
-    if (savedData) {
-        const inputData = JSON.parse(savedData);
-
-        document.getElementById('customer-name').value = inputData.customerName || '';
-        document.getElementById('customer-address').value = inputData.customerAddress || '';
-        document.getElementById('invoice-date').value = inputData.invoiceDate || '';
-        document.getElementById('invoice-number').value = inputData.invoiceNumber || '';
-        document.getElementById('notes').value = inputData.notes || '';
-    }
-}
-
-function clearInvoice() {
-    const inputElements = [
-        'customer-name',
-        'customer-address',
-        'invoice-date',
-        'invoice-number',
-        'notes'
-    ];
-
-    inputElements.forEach((elementId) => {
-        document.getElementById(elementId).value = '';
-    });
-
-    const partsTableBody = document.querySelector('.parts-table tbody');
-    partsTableBody.innerHTML = '';
-
-    const laborTableBody = document.querySelector('.labor-table tbody');
-    laborTableBody.innerHTML = '';
-
-    document.getElementById('subtotal').value = '';
-    document.getElementById('tax-percent').value = '';
-    document.getElementById('tax').value = '';
-    document.getElementById('total').value = '';
-
-    document.getElementById('remaining').checked = false;
-    document.querySelector('.remaining').style.display = 'none';
-
-    document.getElementById('price-difference').innerText = '';
-
-    localStorage.removeItem('inputData');
-}
-
-printButton.addEventListener('click', printInvoice);
-
-saveButton.addEventListener('click', async () => {
-    const invoiceData = await saveInvoice();
-    if (invoiceData) {
-        downloadObjectAsJson(invoiceData, 'invoice');
-    }
-});
-
-addLaborRowButton.addEventListener('click', addLaborRow);
-
-importFileInput.addEventListener('change', async (event) => {
-    const file = event.target.files[0];
-
-    if (file) {
-        try {
-            const invoiceData = await readFileAsJSON(file);
-
-            if (invoiceData) {
-                document.getElementById('customer-name').value = invoiceData.customerName;
-                document.getElementById('customer-address').value = invoiceData.customerAddress;
-                document.getElementById('invoice-date').value = invoiceData.invoiceDate;
-                document.getElementById('invoice-number').value = invoiceData.invoiceNumber;
-                document.getElementById('notes').value = invoiceData.notes;
-
-                const partsTableBody = document.querySelector('.parts-table tbody');
-                partsTableBody.innerHTML = '';
-                invoiceData.parts.forEach((part) => {
-                    const partName = part.part;
-                    if (!partsTableBody.querySelector(`[data-part="${partName}"]`)) {
-                        addPartRow(partName);
-                    }
-                    const newRow = partsTableBody.querySelector(`[data-part="${partName}"]`);
-                    newRow.querySelector('input[name="part-quantity"]').value = part.quantity;
-                    newRow.querySelector('input[name="part-price"]').value = part.price;
-                    updatePartRowTotal(newRow);
-                });
-
-                const laborTableBody = document.querySelector('.labor-table tbody');
-                laborTableBody.innerHTML = '';
-                invoiceData.labor.forEach((labor) => {
-                    addLaborRow();
-                    const newRow = document.querySelector('.labor-table tbody tr:last-child');
-                    newRow.querySelector('input[name="labor-description"]').value = labor.description;
-                    newRow.querySelector('input[name="labor-hours"]').value = labor.hours;
-                    newRow.querySelector('input[name="labor-rate"]').value = labor.rate;
-                    updateTotals();
-                });
-
-                document.getElementById('tax-percent').value = invoiceData.taxPercent;
-                updateTotals();
-            } else {
-                console.error('Invalid JSON data in the imported file');
-            }
-        } catch (error) {
-            console.error('Error reading the imported file:', error);
-        }
-    }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    loadDatabase();
-    taxPercentInput.addEventListener('input', updateTotals);
-    updatePriceDifference();
-});
-
-document.addEventListener('input', function (event) {
-    if (event.target.classList.contains('actual-price-input') || event.target.classList.contains('part-price-input')) {
-        console.log('Part price input or actual price input changed');
-        updatePriceDifference();
-        updateTotals();
-    }
-});
-
-document.getElementById('remaining').addEventListener('change', function () {
-    var remainingDiv = document.querySelector('.remaining');
-
-    if (this.checked) {
-        remainingDiv.style.display = 'block';
-    } else {
-        remainingDiv.style.display = 'none';
-    }
-});
-
-window.onload = function () {
-    var checkbox = document.getElementById('remaining');
-    var remainingDiv = document.querySelector('.remaining');
-    remainingDiv.style.display = checkbox.checked ? 'block' : 'none';
-}
-
-document.getElementById('customer-name').addEventListener('input', saveDataToLocalStorage);
-document.getElementById('customer-address').addEventListener('input', saveDataToLocalStorage);
-document.getElementById('invoice-date').addEventListener('input', saveDataToLocalStorage);
-document.getElementById('invoice-number').addEventListener('input', saveDataToLocalStorage);
-document.getElementById('notes').addEventListener('input', saveDataToLocalStorage);
-
-
+// Initialize the page
 document.addEventListener('DOMContentLoaded', () => {
     loadDatabase();
     taxPercentInput.addEventListener('input', updateTotals);
@@ -468,4 +275,10 @@ document.addEventListener('DOMContentLoaded', () => {
     loadDataFromLocalStorage();
 });
 
+// Function to clear the invoice
+function clearInvoice() {
+    // ... (rest of the code for clearing the invoice)
+}
+
+// Event listener for the "Clear" button
 document.getElementById('clear-button').addEventListener('click', clearInvoice);
