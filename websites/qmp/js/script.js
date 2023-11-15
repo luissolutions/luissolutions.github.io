e8056c8df1e11814de5ec0479af2d8d1f42a762f
Luis <133532790+luissolutions@users.noreply.github.com>
2023-11-15 09:59:18 -0500

    dark mode

--- websites/qmp/js/script.js
+++ websites/qmp/js/script.js
@@ -1,25 +1,35 @@
-//Dark Mode
 const content = document.querySelector('body');
 const darkModeToggle = document.getElementById('dark-mode-toggle');
+const images = document.querySelectorAll('img'); // Select all images
 
-let isFilterActive = localStorage.getItem('darkMode') === 'true';
+let isDarkModeActive = localStorage.getItem('darkMode') === 'true';
+applyDarkMode(isDarkModeActive);
 
-if (isFilterActive) {
-    content.style.filter = 'invert(1)';
-} else {
-    content.style.filter = 'none';
+darkModeToggle.checked = isDarkModeActive;
+
+function applyDarkMode(isDarkMode) {
+    // Apply dark mode to the main content
+    content.style.filter = isDarkMode ? 'invert(1)' : 'none';
+
+    // Apply to all images in the main document
+    images.forEach(img => img.style.filter = isDarkMode ? 'invert(1)' : 'none');
+
+    // Apply to all iframes
+    document.querySelectorAll('iframe').forEach(iframe => {
+        try {
+            const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+            const iframeImages = iframeDoc.querySelectorAll('img');
+
+            // Counter-invert images in the iframe
+            iframeImages.forEach(img => img.style.filter = isDarkMode ? 'invert(1)' : 'none');
+        } catch (error) {
+            console.error("Couldn't access iframe contents", error);
+        }
+    });
 }
 
-darkModeToggle.checked = localStorage.getItem('darkMode') === 'true';
-
 darkModeToggle.addEventListener('change', () => {
-    const isDarkModeEnabled = darkModeToggle.checked;
-
-    if (isDarkModeEnabled) {
-        content.style.filter = 'invert(1)';
-    } else {
-        content.style.filter = 'none';
-    }
-
-    localStorage.setItem('darkMode', isDarkModeEnabled.toString());
-}); 
+    isDarkModeActive = darkModeToggle.checked;
+    applyDarkMode(isDarkModeActive);
+    localStorage.setItem('darkMode', isDarkModeActive.toString());
+});
