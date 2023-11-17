92182b256864d151d94ecddd2c332b76f09aa36f
Luis <133532790+luissolutions@users.noreply.github.com>
2023-11-16 20:25:56 -0500

    img

--- websites/honeydo/js/form.js
+++ websites/honeydo/js/form.js
@@ -10,13 +10,11 @@ document.addEventListener('DOMContentLoaded', function () {
   submitButton.addEventListener('click', function (e) {
     e.preventDefault();
 
-    // Get form field values
     const name = nameInput.value;
     const email = emailInput.value;
     const phone = phoneInput.value;
     const message = messageInput.value;
 
-    // Save form data in LocalStorage
     const formData = {
       name,
       email,
@@ -25,13 +23,10 @@ document.addEventListener('DOMContentLoaded', function () {
     };
     localStorage.setItem('formData', JSON.stringify(formData));
 
-    // Create email body
     const emailBody = `${message} %0D%0A %0D%0A ${name} %0D%0A ${phone} %0D%0A ${email}`;
 
-    // Create the subject line
     const subject = `Contact Request from ${name}`;
 
-    // Use mailto to open the default email client with pre-filled information
-    window.location.href = `mailto:honeydhelper2020@gmail.com?subject=${encodeURIComponent(subject)}&body=${emailBody}`;
+    window.location.href = `mailto:jimmy@honey-dohelper.com?subject=${encodeURIComponent(subject)}&body=${emailBody}`;
   });
 });
