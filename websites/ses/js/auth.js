690a02dceddfc99dabf0ab004631912c0dd03c2a
Luis <133532790+luissolutions@users.noreply.github.com>
2023-12-02 13:49:45 -0500

    nav icons

---
+++ websites/ses/js/auth.js
@@ -0,0 +1,20 @@
+import { auth, onAuthStateChanged, signOut } from './firebase-init.js';
+
+// Check authentication state
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        populateNoteList();
+    } else {
+        window.location.href = 'login.html';
+    }
+});
+
+
+document.getElementById('signOutButton').addEventListener('click', function () {
+    signOut(auth).then(() => {
+        console.log('User signed out');
+        window.location.href = 'login.html';
+    }).catch((error) => {
+        console.error('Sign out error:', error);
+    });
+});
