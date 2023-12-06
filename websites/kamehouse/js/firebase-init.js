37591e1b3638403078af11041db1060d56c1534c
Luis <133532790+luissolutions@users.noreply.github.com>
2023-12-06 09:44:53 -0500

    firebase

---
+++ websites/kamehouse/js/firebase-init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, onValue, push, set, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const firebaseConfig = {
+  databaseURL: "https://playground-e3690-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+export {
+  database, getDatabase, ref, onValue, push, set, remove
+};
