b044675d9ce5c9311c51a48bf93ee95d4883c50f
Luis <133532790+luissolutions@users.noreply.github.com>
2023-12-09 20:43:02 -0500

    assets change

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,7 +1,7 @@
 import firebaseConfig from "./firebase-config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-import { getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update, limitToLast, query } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -9,5 +9,5 @@ const database = getDatabase(app);
 
 export {
   app, database, auth, getAuth, onAuthStateChanged, getDatabase, ref, onValue, set, push, get, off, remove, runTransaction,
-  update, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword
+  update, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, limitToLast, query
 };
