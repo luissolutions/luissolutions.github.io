814b9f44232868d1823ac7476e1649af69444e74
Luis <133532790+luissolutions@users.noreply.github.com>
2023-12-09 19:54:03 -0500

    assets folder

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,16 +1,7 @@
+import firebaseConfig from "./firebase-config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCv2cQGWeXS-w7psrQiZD8dn4R7hStmY1o",
-  authDomain: "persinfo-df93f.firebaseapp.com",
-  databaseURL: "https://persinfo-df93f-default-rtdb.firebaseio.com",
-  projectId: "persinfo-df93f",
-  storageBucket: "persinfo-df93f.appspot.com",
-  messagingSenderId: "218680336647",
-  appId: "1:218680336647:web:7786091136b9e6b28565a2"
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
