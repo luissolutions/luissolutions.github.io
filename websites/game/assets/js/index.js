c0c88a40d4624a3e7a1a6b715f7316299378a3ab
Luis <133532790+luissolutions@users.noreply.github.com>
2024-01-22 19:40:34 -0500

    timer

--- websites/game/assets/js/index.js
+++ websites/game/assets/js/index.js
@@ -1,38 +1,3 @@
-let startTime;
-let elapsedTime = 0;
-let timerInterval;
-
-function timeToString(time) {
-    let diffInHrs = time / 3600000;
-    let hh = Math.floor(diffInHrs);
-
-    let diffInMin = (diffInHrs - hh) * 60;
-    let mm = Math.floor(diffInMin);
-
-    let diffInSec = (diffInMin - mm) * 60;
-    let ss = Math.floor(diffInSec);
-
-    let diffInMs = (diffInSec - ss) * 100;
-    let ms = Math.floor(diffInMs);
-
-    let formattedMM = mm.toString().padStart(2, "0");
-    let formattedSS = ss.toString().padStart(2, "0");
-    let formattedMS = ms.toString().padStart(2, "0");
-
-    return `${formattedMM}:${formattedSS}:${formattedMS}`;
-}
-
-function startTimer() {
-    startTime = Date.now() - elapsedTime;
-    timerInterval = setInterval(function printTime() {
-        elapsedTime = Date.now() - startTime;
-        document.getElementById("timer").innerText = timeToString(elapsedTime);
-    }, 10);
-}
-
-function stopTimer() {
-    clearInterval(timerInterval);
-}
 
 function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
@@ -87,6 +52,4 @@ function loadContent(event, path) {
         });
 }
 
-document.getElementById("startButton").addEventListener("click", startTimer);
-document.getElementById("stopButton").addEventListener("click", stopTimer);
 document.getElementById("toggleSidebar").addEventListener("click", toggleSidebar);
