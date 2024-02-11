e9b24cc82c05b6bc4fb5f23639fd145b1ef2ff93
Luis <133532790+luissolutions@users.noreply.github.com>
2024-02-10 21:18:33 -0500

    game

--- websites/game/assets/js/index.js
+++ websites/game/assets/js/index.js
@@ -50,8 +50,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     toggleButton.addEventListener('click', () => {
         sidebar.classList.toggle('hidden');
 
+        var button = document.getElementById('toggleSidebar');
+
         // Check if sidebar is now visible
         if (!sidebar.classList.contains('hidden')) {
+            button.classList.toggle('xbutton');
+            button.innerText = "Close Sidebar";
             // Delay content animation until sidebar animation completes
             setTimeout(() => {
                 contentItems.forEach(item => {
@@ -59,6 +63,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 });
             }, 500); // Adjust timing to match sidebar animation
         } else {
+            button.classList.toggle('xbutton')
+            button.innerText = "Open Sidebar";
             // Immediately hide content if sidebar is hidden
             contentItems.forEach(item => {
                 item.classList.remove('visible');
