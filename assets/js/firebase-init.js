c66c9401e3a5f2730004ad8bd687735896608707
Luis <133532790+luissolutions@users.noreply.github.com>
2024-09-13 09:18:54 -0400

    images

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -1,13 +1,13 @@
-// firebase-init.js
-
 import { firebaseConfig } from "./firebase-config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, EmailAuthProvider, reauthenticateWithCredential, updatePassword, updateProfile } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update, limitToLast, query } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL, listAll } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
+const storage = getStorage(app);
 
 function initializeAuth(appInstance) {
   document.getElementById('login-form').addEventListener('submit', function (event) {
@@ -53,4 +53,4 @@ function initializeAuth(appInstance) {
   });
 }
 
-export { app, database, auth, getAuth, onAuthStateChanged, getDatabase, ref, onValue, set, push, get, off, remove, runTransaction, update, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, limitToLast, query, EmailAuthProvider, reauthenticateWithCredential, updatePassword, updateProfile, initializeAuth };
+export { app, database, auth, storage, getAuth, onAuthStateChanged, getDatabase, ref, onValue, set, push, get, off, remove, runTransaction, update, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, limitToLast, query, EmailAuthProvider, reauthenticateWithCredential, updatePassword, updateProfile, initializeAuth, getStorage, storageRef, uploadBytes, getDownloadURL, listAll };
