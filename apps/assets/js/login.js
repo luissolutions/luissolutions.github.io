24659c6db96ee674c5839bf77ecb5f94124cfe34
Luis <133532790+luissolutions@users.noreply.github.com>
2024-11-03 09:27:54 -0500

    uid on learn app

--- apps/assets/js/login.js
+++ apps/assets/js/login.js
@@ -1,14 +1,12 @@
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js"; 
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-
 import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 let currentUserUID = null;
 
 document.addEventListener('DOMContentLoaded', () => {
-    const auth = getAuth(app);
-
     const loginForm = document.getElementById('login-form');
     const logoutButton = document.getElementById('logout');
     const emailInput = document.getElementById('username');
@@ -21,6 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         signInWithEmailAndPassword(auth, username, password)
             .then((userCredential) => {
+                currentUserUID = userCredential.user.uid;
+                localStorage.setItem("currentUserUID", currentUserUID);
                 loginForm.style.display = 'none';
                 logoutButton.style.display = 'flex';
                 window.location.reload();
@@ -34,23 +34,34 @@ document.addEventListener('DOMContentLoaded', () => {
     logoutButton.addEventListener('click', () => {
         signOut(auth)
             .then(() => {
+                currentUserUID = null;
+                localStorage.removeItem("currentUserUID");
                 loginForm.style.display = 'flex';
                 logoutButton.style.display = 'none';
                 window.location.reload();
             })
+            .catch((error) => {
+                console.log(`Error [${error.code}]: ${error.message}`);
+            });
     });
 
     onAuthStateChanged(auth, (user) => {
         if (user) {
             currentUserUID = user.uid;
+            localStorage.setItem("currentUserUID", currentUserUID);
             loginForm.style.display = 'none';
             logoutButton.style.display = 'flex';
-            console.log("Logged in as user: ", user.email);
+            console.log("Logged in as user:", user.email);
         } else {
             currentUserUID = null;
+            localStorage.removeItem("currentUserUID");
             loginForm.style.display = 'flex';
             logoutButton.style.display = 'none';
             console.log("No user is logged in.");
         }
     });
 });
+
+export function getCurrentUserUID() {
+    return localStorage.getItem("currentUserUID");
+}
