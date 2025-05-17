4c671da65ff80a6b6f6b64a61036d63d9b8045b8
Luis <133532790+luissolutions@users.noreply.github.com>
2025-05-17 11:57:50 -0400

    ondrive file uploader odgallery

--- apps/assets/js/onenoteGalleryAuth.js
+++ apps/assets/js/onenoteGalleryAuth.js
@@ -1,5 +1,5 @@
-const clientId = "f5ef88e6-7af0-40af-ae6a-5a9d3342360e"; // same
-const tenantId = "f9cb31b8-4d87-4d78-89f4-636d4e6f6509"; // same
+const clientId = "f5ef88e6-7af0-40af-ae6a-5a9d3342360e";
+const tenantId = "f9cb31b8-4d87-4d78-89f4-636d4e6f6509";
 const redirectUri = `${window.location.origin}${window.location.pathname}`;
 let accessToken;
 
@@ -17,8 +17,10 @@ function extractAccessTokenFromUrl() {
     if (token) {
         accessToken = token;
         storage.setAccessToken(token);
-        console.log("Access Token:", accessToken);
-        alert("Successfully logged in!");
+        if (!sessionStorage.getItem("loginNotified")) {
+            alert("Successfully logged in!");
+            sessionStorage.setItem("loginNotified", "true");
+        }
         history.replaceState(null, null, window.location.pathname);
     } else {
         alert("Unable to retrieve access token. Check your Azure configuration.");
@@ -40,17 +42,39 @@ function initializeAccessToken() {
     }
 }
 
-function loginToMicrosoft() {
+async function loginToMicrosoft() {
     const authUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(
         redirectUri
     )}&scope=${encodeURIComponent("Files.ReadWrite.All User.Read")}&response_mode=fragment`;
     window.location.href = authUrl;
 }
 
+async function getUserInfo() {
+    if (!accessToken) return null;
+
+    const res = await fetch("https://graph.microsoft.com/v1.0/me", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
+
+    if (!res.ok) {
+        console.error("Failed to get user info", await res.text());
+        return null;
+    }
+
+    return await res.json();
+}
+
 function logoutFromMicrosoft() {
     storage.clearAccessToken();
     accessToken = null;
+    sessionStorage.removeItem("loginNotified");
     alert("Logged out successfully.");
+}
+
+function isLoggedIn() {
+    return !!accessToken;
 }
 
 function setupAuthEventListeners() {
@@ -61,4 +85,4 @@ function setupAuthEventListeners() {
 initializeAccessToken();
 setupAuthEventListeners();
 
-export { accessToken, loginToMicrosoft, logoutFromMicrosoft };
+export { accessToken, loginToMicrosoft, logoutFromMicrosoft, getUserInfo, isLoggedIn };
