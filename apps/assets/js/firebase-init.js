d9e979a30ba3420e2e97dca3fb3da0a5ef782e92
Luis <133532790+luissolutions@users.noreply.github.com>
2025-06-07 18:37:34 -0400

    livelearn fixed answer counter

--- apps/assets/js/firebase-init.js
+++ apps/assets/js/firebase-init.js
@@ -3,7 +3,7 @@ import { firebaseConfig } from "../../../assets/js/firebase-config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, initializeAuth, signOut, createUserWithEmailAndPassword, EmailAuthProvider, reauthenticateWithCredential, updatePassword, updateProfile, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import {
-  getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update, limitToLast, query
+  getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update, limitToLast, query, child
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import {
   getStorage, ref as storageRef, uploadBytes, getDownloadURL, listAll, uploadBytesResumable, deleteObject, updateMetadata, getBlob, getBytes 
@@ -18,7 +18,7 @@ const storage = getStorage(app);
 export {
   app, database, auth, storage,
   getAuth, onAuthStateChanged, getDatabase, ref, onValue, set, push, get, off, remove,
-  runTransaction, update, signOut, createUserWithEmailAndPassword, limitToLast, query,
+  runTransaction, update, signOut, createUserWithEmailAndPassword, limitToLast, query, child,
   EmailAuthProvider, reauthenticateWithCredential, updatePassword, updateProfile, updateMetadata,
   getStorage, storageRef, uploadBytes, getDownloadURL, listAll, signInWithEmailAndPassword,
   uploadBytesResumable, deleteObject, getFunctions, httpsCallable, initializeAuth, getBlob, getBytes 
