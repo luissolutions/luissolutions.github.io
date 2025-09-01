1e7ace8e7bb41a30be94f88c7ccfce8b5ff99362
Luis <133532790+luissolutions@users.noreply.github.com>
2025-09-01 01:01:07 -0400

    added pdf upload to learnapp

--- apps/assets/js/firebase-init.js
+++ apps/assets/js/firebase-init.js
@@ -6,7 +6,7 @@ import {
   getDatabase, ref, onValue, set, get, off, remove, runTransaction, push, update, limitToLast, query, child
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import {
-  getStorage, ref as storageRef, uploadBytes, getDownloadURL, listAll, uploadBytesResumable, deleteObject, updateMetadata, getBlob, getBytes 
+  getStorage, ref as storageRef, uploadBytes, getDownloadURL, listAll, uploadBytesResumable, deleteObject, getMetadata, updateMetadata, getBlob, getBytes 
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
 import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-functions.js";
 
@@ -17,7 +17,7 @@ const storage = getStorage(app);
 
 export {
   app, database, auth, storage,
-  getAuth, onAuthStateChanged, getDatabase, ref, onValue, set, push, get, off, remove,
+  getAuth, onAuthStateChanged, getDatabase, ref, onValue, set, push, get, off, remove, getMetadata,
   runTransaction, update, signOut, createUserWithEmailAndPassword, limitToLast, query, child,
   EmailAuthProvider, reauthenticateWithCredential, updatePassword, updateProfile, updateMetadata,
   getStorage, storageRef, uploadBytes, getDownloadURL, listAll, signInWithEmailAndPassword,
