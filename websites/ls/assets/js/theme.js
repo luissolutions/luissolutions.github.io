815516a6889d520f0c6faebb083f70a50ae81c72
Luis <133532790+luissolutions@users.noreply.github.com>
2026-05-25 11:38:40 -0400

    removed ls

--- websites/ls/assets/js/theme.js
+++
@@ -1,9 +0,0 @@
-
-document.body.addEventListener('change', function (e) {
-  if (e.target.closest('.theme-content')) {
-    const theme = e.target.value;
-    const link = document.querySelector('link[rel="stylesheet"]');
-    link.href = theme;
-    localStorage.setItem('theme', theme);
-  }
-});
